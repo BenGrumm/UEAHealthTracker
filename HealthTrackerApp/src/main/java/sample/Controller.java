98a8dba5115aff8099285940965cf0cb5b0eccbc
Outlands <elevatedlegendz@gmail.com>
2021-03-29 23:31:06 +0100

    Testing

--- HealthTrackerApp/src/main/java/sample/Controller.java
+++ HealthTrackerApp/src/main/java/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
+
 public class Controller {
+
+    @FXML
+    private Menu logIn;
+
+    @FXML
+    private Button wadbutton;
+
+    @FXML
+    void changeScene(ActionEvent event) {
+
+        /*Stage stage = (Stage) wadbutton.getScene().getWindow();
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("/testing2.fxml"));
+            stage.setScene(new Scene(root, 400,400));
+        }catch (Exception e){
+
+        }*/
+    }
+
 }
+
+
--- HealthTrackerApp/src/main/java/sample/GUI.java
+++ HealthTrackerApp/src/main/java/sample/GUI.java
@@ -10,10 +10,18 @@ public class GUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+<<<<<<< Updated upstream
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
+=======
+        Parent root = FXMLLoader.load(getClass().getResource("/testing.fxml"));
+        Parent root2 = FXMLLoader.load(getClass().getResource("/testing2.fxml"));
+        primaryStage.setTitle("Hello World 5");
+>>>>>>> Stashed changes
         primaryStage.setScene(new Scene(root, 300, 275));
+
         primaryStage.show();
+
     }
 
 }
