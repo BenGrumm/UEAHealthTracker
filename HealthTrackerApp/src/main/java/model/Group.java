41339b51c74c41f6b865e900a6f46be21930b5e1
Alex-Clifford <57949510+Alex-Clifford@users.noreply.github.com>
2021-04-02 18:02:45 +0100

    Added Group Java Class
    Added a basic group class with some methods

---
+++ HealthTrackerApp/src/main/java/model/Group.java
@@ -0,0 +1,115 @@
+package model;
+
+/**
+ * This class is to store the information associated with groups.
+ * This will store an ID, Name and Description and Size.
+ * @author Alexander Clifford
+ **/
+
+public class Group {
+
+    private int iD, size;
+    private String name, description;
+
+    /** Getters **/
+    public int getiD() {
+        return iD;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    /** Setters **/
+
+    //Need to make sure does not already exist
+    private void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /** Additional Methods **/
+    public void generateiD(){
+        int generatedID = 0;
+
+        //Use database to work out unique ID
+
+        this.iD = generatedID;
+    }
+
+    public void incrementSize() {
+        this.size = size + 1;
+    }
+
+
+
+
+    /**Constructors**/
+
+    //Without ID. (Preferred)
+    public Group(int size, String name, String description) {
+        generateiD();
+        this.size = size;
+        this.name = name;
+        this.description = description;
+    }
+
+    //With ID
+    public Group(int iD, int size, String name, String description) {
+        setiD(iD);
+        this.size = size;
+        this.name = name;
+        this.description = description;
+    }
+
+    /** Test Harness **/
+
+    public static void main(String[] args) {
+        Group group1 = new Group(1,1,"Test1","");
+        Group group2 = new Group(1,"Test2","Desc");
+
+        System.out.println(group1.getiD());
+        System.out.println(group1.getName());
+        System.out.println(group1.getDescription());
+        System.out.println(group1.getSize());
+
+        System.out.println(group2.getiD());
+        group2.setiD(10);
+        System.out.println(group2.getiD());
+
+        group1.setName("Changed");
+        group1.setDescription("ChangedDesc");
+        group1.setSize(2);
+
+        System.out.println(group1.getiD());
+        System.out.println(group1.getName());
+        System.out.println(group1.getDescription());
+        System.out.println(group1.getSize());
+
+        group1.incrementSize();
+
+        System.out.println(group1.size);
+
+    }
+
+
+}
