9387cb778722e83cd9686b5ebbfd2de89b0860b8
Alex-Clifford <57949510+Alex-Clifford@users.noreply.github.com>
2021-05-05 11:58:54 +0100

    Finished GroupDBHelper Methods

--- HealthTrackerApp/src/main/java/model/GoalDBHelper.java
+++ HealthTrackerApp/src/main/java/model/GoalDBHelper.java
@@ -1,5 +1,7 @@
 package model;
 
+
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -128,17 +130,81 @@ public class GoalDBHelper {
         return true;
     }
 
-    public void dupeGroupGoal(){
+    /**
+     * MAKE SURE USER HASNT GOT GOAL BEFROE CALLING
+     * @param goal
+     * @param userID
+     */
+    public void dupeGroupGoal(Goal goal, Integer userID){
+        if(goal.getDateStart().toString().equals("0")){
+            addGoal(goal.getName(), goal.getGoalType(), goal.getProgress(), goal.getTarget(), userID.toString(), goal.getGoalID());
+        }
+        else {
+            addGoal(goal.getName(), goal.getGoalType(), goal.getProgress(), goal.getTarget(), goal.getDateStart(), goal.getDateEnd(), userID.toString(), goal.getGoalID());
+        }
+        System.out.println("Hi");
+    }
 
-        //create a new goal associated with userid and use the id of the groupgoal for the copiedfrom value.
+    public boolean userHasGoal(int goalID,int userID) {
+        ArrayList<Integer> goalIDs = new ArrayList<>();
+
+        String getGoalIDsSQL = "SELECT " + GUCOLUMN_GOALID + " FROM " + GUTABLE_NAME + " WHERE " + GUCOLUMN_USERID + " = " + userID + ";";
+        try {
+            ResultSet results = db.selectQuery(getGoalIDsSQL);
+
+            while (results.next()) {
+                int gID = results.getInt(GGCOLUMN_GOALID);
+                goalIDs.add(gID);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        boolean ret = true;
+
+        for(int x=0; x< goalIDs.size();x++){
+            String getGoalIDSQL = "SELECT " + COLUMN_NAME + " FROM " + TABLE_NAME + " WHERE " + COLUMN_GOALID + " = " + goalIDs.get(x) + " AND " + COLUMN_COPIEDFROM + " = " + goalID + ";";
+            System.out.println(getGoalIDSQL);
+            try {
+                ResultSet results = db.selectQuery(getGoalIDSQL);
+                if (!results.isBeforeFirst()) {
+                    ret = false;
+                }
+                results.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return ret;
     }
 
     public void removeGoal(){
 
     }
-    public void removeGroupGoal(){
+
+    public void removeGroupGoal(int goalID, int groupID){
+
+        String deleteGroupGoalSQL = "DELETE FROM " + GGTABLE_NAME + " WHERE " + GGCOLUMN_GROUPID + " = " + groupID + " AND " + GGCOLUMN_GOALID + " = " + goalID + ";";
+        try {
+            db.deleteData(deleteGroupGoalSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String removeGroupGoalFromGoalsSQL = "UPDATE " + TABLE_NAME + " SET " + COLUMN_COPIEDFROM + " = " + 0 + " WHERE " + COLUMN_COPIEDFROM + " = " + goalID + ";";
+        System.out.println(removeGroupGoalFromGoalsSQL);
+        try {
+            db.updateTable(removeGroupGoalFromGoalsSQL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+
         //fetch the goalid of the group goal. treat this at the copiedfrom value to check for.
-        //cascade removes group goal from group goal table.
+        //Xcascade removes group goal from group goal table.
         //go through the goal table and change all goals that had the copiedfrom value to 0.
     }
 
@@ -153,9 +219,77 @@ public class GoalDBHelper {
         return new ArrayList<Goal>();
     }
 
-    public ArrayList<Goal> getGoalsByGroupId(){
-        return new ArrayList<Goal>();
+    /** SEEMS TO WORK, ONLY IF GOALS ARE SAVED AS 0 IN DATE IF THEY DON'T USE THEM **/
+    public ArrayList<Goal> getGoalsByGroupId(int groupID){
+        ArrayList<Integer> goalIDs = new ArrayList<>();
+        ArrayList<Goal> groupGoals = new ArrayList<>();
+
+        String getGoalIDsSQL = "SELECT " + GGCOLUMN_GOALID + " FROM " + GGTABLE_NAME + " WHERE "+ GGCOLUMN_GROUPID + " = " + groupID + ";";
+        try {
+            ResultSet results = db.selectQuery(getGoalIDsSQL);
+            while (results.next()) {
+                int goalID = results.getInt(GGCOLUMN_GOALID);
+                goalIDs.add(goalID);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        for(int x=0; x< goalIDs.size();x++){
+            groupGoals.add(getGoalByID(goalIDs.get(x)));
+        }
+
+        return groupGoals;
     }
+
+
+    public Goal getGoalByID(int goalID){
+
+        String getGoalSQL = "SELECT * FROM " + TABLE_NAME + " WHERE " + COLUMN_GOALID + " = " + goalID + ";";
+        System.out.println(getGoalSQL);
+        try {
+            ResultSet results = db.selectQuery(getGoalSQL);
+
+            boolean hasDates = true;
+            LocalDate startdate = null,enddate = null;
+
+            while (results.next()) {
+                int id = results.getInt(COLUMN_GOALID);
+                String name = results.getString(COLUMN_NAME);
+                String goaltype = results.getString(COLUMN_GOALTYPE);
+                float progress = results.getFloat(COLUMN_PROGRESS);
+                float target = results.getFloat(COLUMN_TARGET);
+                if(results.getString(COLUMN_DATESTART).equals("0")){
+                    hasDates = false;
+                }
+                else {
+                    startdate = LocalDate.parse(results.getString(COLUMN_DATESTART));
+                }
+                if(results.getString(COLUMN_DATEEND).equals("0")){
+                    hasDates = false;
+                }
+                else {
+                    enddate = LocalDate.parse(results.getString(COLUMN_DATEEND));
+                }
+                int copiedfrom = results.getInt(COLUMN_COPIEDFROM);
+
+                Goal.goal gt = Goal.goal.valueOf(goaltype);
+
+                if (hasDates) {
+                    return new Goal(id, name, gt, progress, target, startdate, enddate, copiedfrom);
+                }
+                else{
+                    return new Goal(id, name, gt, progress, target, copiedfrom);
+                }
+            }
+
+            results.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 
     public boolean addGroupGoal(String name, Goal.goal goalType, float progress, float target, LocalDate startDate, LocalDate endDate, String groupID, int copiedFrom){
         try {
@@ -188,8 +322,38 @@ public class GoalDBHelper {
         return true;
     }
 
+    public int getNumberOfGoalsForAGroup(int groupID){
+        ArrayList<Integer> goalIDs = new ArrayList<>();
+
+        String getGoalIDsSQL = "SELECT " + GGCOLUMN_GOALID + " FROM " + GGTABLE_NAME + " WHERE "+ GGCOLUMN_GROUPID + " = " + groupID + ";";
+        try {
+            ResultSet results = db.selectQuery(getGoalIDsSQL);
+            while (results.next()) {
+                int goalID = results.getInt(GGCOLUMN_GOALID);
+                goalIDs.add(goalID);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return goalIDs.size();
+    }
+
+
     public static void main(String[] args) {
         GoalDBHelper GDBH = new GoalDBHelper();
+
+        ArrayList<Goal> g = GDBH.getGoalsByGroupId(1);
+
+        for(int x=0; x<g.size(); x++){
+            System.out.println(g.get(x).getGoalID()+ ": " + g.get(x).getName());
+        }
+
+        GDBH.dupeGroupGoal(g.get(2), 23);
+        System.out.println("Group size: " + GDBH.getNumberOfGoalsForAGroup(1));
+
+        System.out.println("is " + GDBH.userHasGoal(3,23));
+        System.out.println(GDBH.userHasGoal(3,24));
     }
 
 }
